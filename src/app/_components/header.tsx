901a9636175b3a3195175d035ddd549cbeda9839
abiolafasanya <hezekiahfasanya@gmail.com>
2023-10-02 07:48:49 +0100

    header link

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -6,10 +6,10 @@ const Header = ({className}: {className?: string}) => {
   const styles = 'flex flex-row justify-between container mx-auto py-5 px-7 items-center'
   return (
     <div className={cn(styles, className)}>
-      <div className='flex gap-2 items-center'>
+      <Link href="/" className='flex gap-2 items-center'>
         <Image src='/logo.png' alt='logo' width={48} height={48} />
         <h3 className='text-2xl text-main-500 font-semibold'>HelpMeOut</h3>
-      </div>
+      </Link>
       <menu className='flex gap-4'>
         <Link href={'/features'} className=''>
           Features
