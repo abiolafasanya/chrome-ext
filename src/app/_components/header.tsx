67bc2d1bc14643522cf7562ef191ff6fc97d92a0
abiolafasanya <hezekiahfasanya@gmail.com>
2023-10-03 00:00:34 +0100

    refactor

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const Header = ({className}: {className?: string}) => {
-  const styles = 'flex flex-row justify-between container mx-auto py-5 px-7 items-center'
+  const styles = 'flex flex-row justify-between flex-wrap container mx-auto py-5 px-7 items-center'
   return (
     <div className={cn(styles, className)}>
       <Link href="/" className='flex gap-2 items-center'>
