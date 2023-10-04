1302ebfd046904e85d67bbb442016fd0efd8a573
abiolafasanya <hezekiahfasanya@gmail.com>
2023-10-04 03:52:55 +0100

    refactor: moved script to content.js

--- extension/background.js
+++ extension/background.js
@@ -1,177 +1,90 @@
-chrome.runtime.onMessage.addListener(async function (
-  message,
-  sender,
-  response
-) {
-  console.log(message, sender, response);
+console.log("background is watching!")
 
-  if (message.command === 'show') {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const activeTab = tabs[0];
-      if (activeTab) {
-        chrome.scripting.executeScript({
-          target: { tabId: activeTab.id },
-          function: (message) => {
-            // Here, you can handle the message in the content script
-            console.log(message);
-            var init = () => {
-              const recordButton = document.createElement('div');
-              recordButton.id = 'record';
-              recordButton.className =
-                'flex items-center justify-center rounded-full p-2 h-10 w-10 cursor-pointer bg-white';
-              const recordImg = document.createElement('img');
-              recordImg.src =
-                'https://res.cloudinary.com/abiolafasanya/image/upload/v1696059301/pause_kn1q0u.svg';
-              recordImg.alt = 'pause';
-              recordImg.className = 'w-[16px] h-[16px]';
-              recordButton.appendChild(recordImg);
+ // start recording button
+//  async function startRecording() {
+//   try {
+//     let videoInput;
+//     console.log({ defaultScreen });
+//     if (defaultScreen === "current_tab") {
+//       // Capture current tab
+//       const streamId = JSON.parse(localStorage.getItem("@hmo_tab_stream_id"));
+//       videoInput = await navigator.mediaDevices.getUserMedia({
+//         audio: {
+//           mandatory: {
+//             chromeMediaSource: "tab",
+//             chromeMediaSourceId: streamId,
+//           },
+//         },
+//         video: {
+//           mandatory: {
+//             chromeMediaSource: "tab",
+//             chromeMediaSourceId: streamId,
+//           },
+//         },
+//       });
+//     } else {
+//       // capture entire screen
+//       videoInput = await navigator.mediaDevices.getDisplayMedia({
+//         video: { mediaSource: "screen" },
+//         audio: audioState,
+//       });
+//     }
 
-              const stopButton = document.createElement('div');
-              stopButton.id = 'stop';
-              stopButton.className =
-                'flex items-center justify-center rounded-full p-2 h-10 w-10 cursor-pointer bg-white';
-              const stopImg = document.createElement('img');
-              stopImg.src =
-                'https://res.cloudinary.com/abiolafasanya/image/upload/v1696059300/stop_eiqvni.svg';
-              stopImg.alt = '';
-              stopImg.className = 'w-[20px] h-[20px]';
-              stopButton.appendChild(stopImg);
+//     let audioInput = await navigator.mediaDevices.getUserMedia({
+//       audio: true,
+//       video: false,
+//     });
 
-              const videoButton = document.createElement('div');
-              videoButton.id = 'video';
-              videoButton.className =
-                'flex items-center justify-center rounded-full p-2 h-10 w-10 cursor-pointer bg-white';
-              const videoImg = document.createElement('img');
-              videoImg.src =
-                'https://res.cloudinary.com/abiolafasanya/image/upload/v1696059299/video_bgajdt.svg';
-              videoImg.alt = '';
-              videoImg.className = 'w-[18px] h-[18px]';
-              videoButton.appendChild(videoImg);
+//     // combine both media video/audio
+//     let combineMedia = new MediaStream([
+//       ...videoInput.getTracks(),
+//       ...audioInput.getTracks(),
+//     ]);
+//     console.log(mediaRecorder);
+//     if (mediaRecorder === null) {
+//       mediaRecorder = new MediaRecorder(combineMedia, {
+//         mimeType: mime,
+//       });
 
-              const audioButton = document.createElement('div');
-              audioButton.id = 'audio';
-              audioButton.className =
-                'flex items-center justify-center rounded-full p-2 h-10 w-10 cursor-pointer bg-white';
-              const audioImg = document.createElement('img');
-              audioImg.src =
-                'https://res.cloudinary.com/abiolafasanya/image/upload/v1696059302/audio_fmh6js.svg';
-              audioImg.alt = '';
-              audioImg.className = 'w-[18px] h-[18px]';
-              audioButton.appendChild(audioImg);
+//       mediaRecorder.addEventListener("dataavailable", async function (e) {
+//         recordedChunks.push(e.data);
 
-              const deleteButton = document.createElement('div');
-              deleteButton.id = 'delete';
-              deleteButton.className =
-                'flex items-center justify-center rounded-full p-2 h-10 w-10 cursor-pointer bg-white/30';
-              const deleteImg = document.createElement('img');
-              deleteImg.src =
-                'https://res.cloudinary.com/abiolafasanya/image/upload/v1696059301/delete_nnglbr.svg';
-              deleteImg.alt = '';
-              deleteImg.className = 'w-[18px] h-[18px]';
-              deleteButton.appendChild(deleteImg);
+//         // stream to backend
+//         const chunk = [e.data];
+//         await streamChunksToServer(chunk);
+//       });
 
-              // Create a container div for the buttons
-              const controlButtonsContainer = document.createElement('div');
-              controlButtonsContainer.className =
-                'w-[400px] flex gap-4 py-4 px-5 justify-center items-center bottom-10  rounded-full bg-[#141414] mx-20 p-3';
+//       mediaRecorder.addEventListener("stop", async function () {
+//         let blob = new Blob(recordedChunks, {
+//           type: recordedChunks[0].type,
+//         });
+//         recordedBlobUrl = URL.createObjectURL(blob);
+//         videoInput.getTracks()[0].stop();
+//         audioInput.getTracks().forEach((track) => {
+//           track.stop();
+//         });
 
-              // Append the buttons to the container
-              controlButtonsContainer.appendChild(recordButton);
-              controlButtonsContainer.appendChild(stopButton);
-              controlButtonsContainer.appendChild(videoButton);
-              controlButtonsContainer.appendChild(audioButton);
-              controlButtonsContainer.appendChild(deleteButton);
+//         // update recording ui component
+//         await resetUIOnRecordStop();
 
-              // Append the container to the desired location in the document
-              const body = document.body.appendChild(controlButtonsContainer);
-              const constraints = {
-                audio: false,
-                video: true,
-              };
+//         // end stream
+//         if (!streamRequestEnded) {
+//           await endStream(hmo_streamVideoId);
+//           streamRequestEnded = true;
+//           window.open(`${CLIENT_URL}/file/${hmo_streamVideoId}`);
+//         }
+//       });
 
-              body.style.margin = '5rem';
-              body.style.position = 'fixed';
-              body.style.zIndex = 100;
-              body.style.top = '450px';
-              body.style.height = '64px';
+//       mediaRecorder.start(1000);
 
-              recordButton.addEventListener('click', () => {
-                // Define your API endpoint where you want to send the screen capture
-                const apiEndpoint = 'https://tabitha-njoki.onrender.com/upload';
-                console.log(apiEndpoint);
-                chrome.desktopCapture.chooseDesktopMedia(
-                  ['screen', 'window'],
-                  async function (id) {
-                    console.log(id);
-
-                    // Capture the screen as a media stream
-                    let mediaStream =
-                      await navigator.mediaDevices.getDisplayMedia({
-                        video: { mediaSource: id },
-                      });
-
-                    // Create a Blob from the stream
-                    const recorder = new MediaRecorder(mediaStream);
-                    let recordedChunks = [];
-
-                    recorder.ondataavailable = (event) => {
-                      if (event.data.size > 0) {
-                        recordedChunks.push(event.data);
-                      }
-                    };
-
-                    recorder.onstop = () => {
-                      // Convert the recorded chunks into a Blob
-                      const blob = new Blob(recordedChunks, {
-                        type: 'video/webm',
-                      });
-
-                      // Send the Blob to your API using the fetch API
-                      fetch(apiEndpoint, {
-                        method: 'POST',
-                        body: { upload: blob },
-                      })
-                        .then((response) => {
-                          if (response.ok) {
-                            console.log(
-                              'Screen recording sent to the API successfully.'
-                            );
-                          } else {
-                            console.error(
-                              'Error sending screen recording to the API.'
-                            );
-                          }
-                        })
-                        .catch((error) => {
-                          console.error(
-                            'Error sending screen recording to the API:',
-                            error
-                          );
-                        });
-                    };
-
-                    // Start recording
-                    recorder.start();
-
-                    // Stop recording after some time (you can customize this duration)
-                    setTimeout(() => {
-                      recorder.stop();
-                      mediaStream.getTracks().forEach((track) => track.stop());
-                    }, 3000); // Stop recording after 10 seconds (adjust as needed)
-                  }
-                );
-              });
-            };
-          },
-          args: [request.message], // Pass the message to the content script
-        });
-      }
-    });
-    // await chrome.scripting.executeScript({
-    //   files: ['contentScript.js'],
-    //   target: { tabId: message.tabId },
-
-    // });
-    response('recieved message');
-  }
-});
+//       stream = audioInput;
+//       return true;
+//     }
+//   } catch (e) {
+//     // window.alert(e);
+//     Toast().error(e);
+//     console.log(e);
+//     console.log(`error starting recorder`);
+//     return false;
+//   }
+// }
--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,3 +1,277 @@
+let isRecording = null;
+let isStopped = null;
 
+const timer = document.createElement('div');
+timer.id = 'timer';
+timer.className = `flex items-center text-md justify-center p-2 cursor-pointer w-28 text-white px-5`;
+timer.innerHTML = `<p class="flex gap-2 items-center"><span>00:00:00</span>  <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="10" height="10" rx="5" fill="#C00404"/>
+        </svg> <span class="border-l border-white">&nbsp;</span>
+        </p>`;
 
-console.log("script")
+const recordButton = document.createElement('button');
+recordButton.id = 'record';
+recordButton.className = `flex items-center justify-center rounded-full p-2 h-10 w-10 cursor-pointer bg-white`;
+const recordImg = document.createElement('img');
+recordImg.src =
+  'https://res.cloudinary.com/abiolafasanya/image/upload/v1696059301/pause_kn1q0u.svg';
+recordImg.alt = 'pause';
+recordImg.className = 'w-[16px] h-[16px]';
+recordButton.appendChild(recordImg);
+
+const stopButton = document.createElement('button');
+stopButton.id = 'stop';
+stopButton.className = `flex items-center justify-center rounded-full p-2 h-10 w-10 cursor-pointer ${
+  isRecording ? 'bg-[#141414]' : 'bg-white'
+}`;
+const stopImg = document.createElement('img');
+stopImg.src =
+  'https://res.cloudinary.com/abiolafasanya/image/upload/v1696059300/stop_eiqvni.svg';
+stopImg.alt = '';
+stopImg.className = 'w-[20px] h-[20px]';
+stopButton.appendChild(stopImg);
+
+const videoButton = document.createElement('button');
+videoButton.id = 'video';
+videoButton.className =
+  'flex items-center justify-center rounded-full p-2 h-10 w-10 cursor-pointer bg-white';
+const videoImg = document.createElement('img');
+videoImg.src =
+  'https://res.cloudinary.com/abiolafasanya/image/upload/v1696059299/video_bgajdt.svg';
+videoImg.alt = '';
+videoImg.className = 'w-[18px] h-[18px]';
+videoButton.appendChild(videoImg);
+
+const audioButton = document.createElement('button');
+audioButton.id = 'audio';
+audioButton.className =
+  'flex items-center justify-center rounded-full p-2 h-10 w-10 cursor-pointer bg-white';
+const audioImg = document.createElement('img');
+audioImg.src =
+  'https://res.cloudinary.com/abiolafasanya/image/upload/v1696059302/audio_fmh6js.svg';
+audioImg.alt = '';
+audioImg.className = 'w-[18px] h-[18px]';
+audioButton.appendChild(audioImg);
+
+const deleteButton = document.createElement('button');
+deleteButton.id = 'delete';
+deleteButton.className =
+  'flex items-center justify-center rounded-full p-2 h-10 w-10 cursor-pointer bg-white/30';
+const deleteImg = document.createElement('img');
+deleteImg.src =
+  'https://res.cloudinary.com/abiolafasanya/image/upload/v1696059301/delete_nnglbr.svg';
+deleteImg.alt = '';
+deleteImg.className = 'w-[18px] h-[18px]';
+deleteButton.appendChild(deleteImg);
+
+// Create a container div for the buttons
+const controlButtonsContainer = document.createElement('div');
+controlButtonsContainer.className =
+  ' flex gap-4 py-4 px-5 justify-center items-center bottom-10  rounded-full bg-[#141414] mx-20 p-3';
+
+// Append the buttons to the container
+controlButtonsContainer.appendChild(timer);
+controlButtonsContainer.appendChild(recordButton);
+controlButtonsContainer.appendChild(stopButton);
+controlButtonsContainer.appendChild(videoButton);
+controlButtonsContainer.appendChild(audioButton);
+controlButtonsContainer.appendChild(deleteButton);
+
+controlButtonsContainer.classList.add('draggable');
+controlButtonsContainer.draggable = true;
+
+document.addEventListener('mouseup', () => {
+  isDragging = false;
+  controlButtonsContainer.style.cursor = 'grab';
+});
+
+const body = document.body.appendChild(controlButtonsContainer);
+
+const constraints = {
+  audio: true,
+  video: true,
+};
+
+const apiEndpoint = 'https://tabitha-njoki.onrender.com/upload';
+body.style.margin = '5rem';
+body.style.position = 'fixed';
+body.style.zIndex = 100;
+body.style.top = '450px';
+body.style.height = '64px';
+
+let recorder;
+let recordedChunks = [];
+let stream = null;
+let audio = null;
+
+console.log({ isRecording, isStopped }, 'checking ');
+
+async function startRecording() {
+  isRecording = true;
+  isStopped = false;
+  console.log({ isRecording, isStopped }, 'checking ');
+  if (recorder && recorder.state === 'recording') {
+    recorder.pause();
+    console.log('recording paused');
+  } else {
+    stream = navigator.mediaDevices.getDisplayMedia({
+      video: true,
+      audio: true,
+    });
+
+    audio = navigator.mediaDevices.getUserMedia({
+      video: false,
+      audio: true,
+    });
+
+    let audioInput = await audio;
+
+    console.log('recording started');
+
+    stream
+      .then(async (mediaStream) => {
+        let media = new MediaStream([
+          ...mediaStream.getTracks(),
+          ...audioInput.getAudioTracks(),
+        ]);
+        recorder = new MediaRecorder(media, {
+          mimeType: 'video/webm;codecs=vp9,opus',
+        });
+        const audioTrack = mediaStream?.getAudioTracks();
+        console.log(audioTrack, 'audio track');
+
+        recorder.ondataavailable = (event) => {
+          if (event.data.size > 0) {
+            recordedChunks.push(event.data);
+          }
+        };
+
+        recorder.start();
+      })
+      .catch((error) => {
+        console.error('Error accessing media devices:', error);
+      });
+  }
+}
+
+async function activateAudio() {
+  try {
+    await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false,
+    });
+  } catch (e) {
+    console.log('no audio');
+  }
+}
+
+async function activateVideo() {
+  try {
+    await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: true,
+    });
+  } catch (e) {
+    console.log('no audio');
+  }
+}
+
+async function stopRecording() {
+  isStopped = true;
+  isRecording = false;
+  console.log({ isRecording, isStopped }, 'checking ');
+  alert('capturing has ended');
+  if (recorder && recorder.state !== 'inactive') {
+    recorder.stop();
+    recorder.onstop = () => {
+      const blob = new Blob(recordedChunks, {
+        mimeType: 'video/webm;codecs=vp9,opus',
+      });
+      console.log();
+      recordedChunks = [];
+
+      if (stream) {
+        stream.then((stream) => {
+          const tracks = stream.getTracks();
+          tracks.forEach((track) => track.stop());
+        });
+        stream = null;
+      }
+
+      if (audio) {
+        audio.then((stream) => {
+            console.log(stream, "audio stream")
+          const tracks = stream.getTracks();
+          tracks.forEach((track) => track.stop());
+        });
+        audio = null;
+      }
+
+      console.log(blob, 'blob  here');
+
+      const formData = new FormData();
+      formData.append(
+        'video',
+        blob,
+        `untitled_video_${new Date().getTime()}.webm`
+      );
+
+      fetch(apiEndpoint, {
+        method: 'POST',
+        body: formData,
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log('Screen recording sent to the API successfully.');
+            const res = response.json();
+            return res;
+          } else {
+            console.error('Error sending screen recording to the API.');
+          }
+        })
+        .then(async (data) => {
+          console.log(data);
+          if (data) {
+            const baseUrl = 'https://chrome-ext-five.vercel.app';
+            window.open(
+              `${baseUrl}/playback?recording=${data.url}&transcript=${data.transcribe_url}&filename=${data.video_name}`
+            );
+          } else throw new Error('An error occured');
+        })
+        .catch((error) => {
+          console.error('Error sending screen recording to the API:', error);
+        });
+    };
+  }
+}
+
+audioButton.addEventListener('click', async (e) => {
+  await activateAudio();
+});
+
+videoButton.addEventListener('click', async (e) => {
+  await activateVideo();
+});
+
+recordButton.addEventListener('click', (e) => {
+  // e.currentTarget.disabled = isRecording; // Disable the button
+  if (isRecording) {
+    recordButton.className = `flex items-center justify-center rounded-full p-2 h-10 w-10 cursor-pointer bg-[#141414]`;
+    recordButton.classList.add('disabled:bg-[#141414]');
+  } else {
+    recordButton.className = `flex items-center justify-center rounded-full p-2 h-10 w-10 cursor-pointer bg-white`;
+  }
+  startRecording();
+});
+
+stopButton.addEventListener('click', (e) => {
+  e.currentTarget.disabled = isStopped;
+  if (isStopped) {
+    stopButton.className = `flex items-center justify-center rounded-full p-2 h-10 w-10 cursor-pointer bg-[#141414]`;
+  } else {
+    stopButton.className = `flex items-center justify-center rounded-full p-2 h-10 w-10 cursor-pointer bg-white`;
+  }
+  setTimeout(() => {
+    stopRecording();
+  }, 1000); // Stop recording after 1 seconds (adjust as needed)
+});
